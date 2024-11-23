640564b16bf914b4693d921e6929450b36b84a3e
Mason Barnes <barnesmason980@gmail.com>
2024-11-22 16:48:41 -0800

    clientID

---
+++ server/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, make_response
+from flask_restful import Resource, Api
+import requests
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+
+
+base = 'https://api.spotify.com'
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+
+class Chart(Resource):
+    def get(self):
+        try:
+            response = requests.get("https://api.deezer.com/chart")
+            if (response.ok):
+                data = response.json()
+                return make_response(data, 200)
+            else:
+                return jsonify({"error": "Failed to fetch chart data"}), 500
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500            
+
+
+
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+
+
+api.add_resource(Chart, '/api/chart')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
