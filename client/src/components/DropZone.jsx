0c68bb3accc5c1d519f2cffe7fedc77802eb117c
Mason Barnes <barnesmason980@gmail.com>
2025-01-05 14:39:03 -0800

    drop div setNodeRef

--- client/src/components/DropZone.jsx
+++ client/src/components/DropZone.jsx
@@ -1,8 +1,12 @@
 import { useDroppable } from "@dnd-kit/core"
 
 function DropZone(){
+    const { setNodeRef, isOver } = useDroppable({ id });
+
     return(
-        <></>
+        <div ref={setNodeRef}>
+
+        </div>
     )
 }
 
