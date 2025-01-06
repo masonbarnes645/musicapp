368a85a5b204c17a459a6590edfc0768b67c6729
Mason Barnes <barnesmason980@gmail.com>
2025-01-06 12:33:06 -0800

    id for node ref

--- client/src/components/DropZone.jsx
+++ client/src/components/DropZone.jsx
@@ -1,10 +1,12 @@
 import { useDroppable } from "@dnd-kit/core"
 import Grid from '@mui/material/Grid2';
 
-function DropZone({ key }) {
-    const { setNodeRef, isOver } = useDroppable({ key });
+
+function DropZone({ id }) {
+    const { setNodeRef, isOver } = useDroppable({ id });
 
     return (
+
         <Grid size={{ xl: 3}} >
             <div style={{width:'100px'}}>
                 <div ref={setNodeRef}>
--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -6,7 +6,7 @@ function Layout() {
 
     return(
         <Grid2 container spacing={.5} sx={{width:'100vw', height:'100vh'}}>
-            {Array(16).fill(null).map((_, index) => <DropZone key={index}/>)}
+            {Array(16).fill(null).map((_, index) => <DropZone key={index} id={index}/>)}
         </Grid2>
 )
 }
