79f578f70b43c6f694a64d9f2968ebfc483feedf
Mason Barnes <barnesmason980@gmail.com>
2025-03-11 12:32:10 -0700

    dropGrid

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,10 +3,11 @@ import DropZone from "./DropZone";
 
 
 function Layout() {
-
+    const dropGrid = Array(18).fill(null).map((_, index) => <DropZone key={index} id={index}/>)
+    console.log(dropGrid)
     return(
         <Grid2 container spacing={.5} sx={{width:'80%'}} >
-            {Array(18).fill(null).map((_, index) => <DropZone key={index} id={index}/>)}
+            {dropGrid}
         </Grid2>
 )
 }
