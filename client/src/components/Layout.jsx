4ddf19a4599ada827dec901885339eec440c211d
Mason Barnes <barnesmason980@gmail.com>
2025-03-17 15:46:10 -0700

    overflow hidden

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -6,10 +6,12 @@ function Layout() {
     const dropGrid = Array(18).fill(null).map((_, index) => <DropZone key={index} id={index}/>)
     console.log(dropGrid)
     return(
-        <Grid2 container spacing={.5} sx={{width:'80%'}} >
+        <Grid2 container spacing={.5} sx={{width:'80%', overflow:'hidden'}} >
             {dropGrid}
         </Grid2>
 )
 }
 
 export default Layout
+
+// on dragend: replace dropzone with widget
